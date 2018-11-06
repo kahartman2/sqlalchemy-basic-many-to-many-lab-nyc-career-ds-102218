25e127330b6912c4135b62e76c230d4274a6aad7
Kayla Hartman <kahartman95@gmail.com>
2018-11-06 16:12:16 -0500

    our solutions

--- models.py
+++ models.py
@@ -6,8 +6,27 @@ Base = declarative_base()
 
 # Write your classes below
 
+class Actor(Base):
+    __tablename__ = 'actors'
 
+    id = Column(Integer, primary_key = True)
+    name = Column(Text)
 
+    roles = relationship('Role', secondary = 'actor_roles')
 
-engine = create_engine('sqlite:///:memory:')
+class Role(Base):
+    __tablename__ = 'roles'
+
+    id = Column(Integer, primary_key = True)
+    character = Column(Text)
+
+    actors = relationship('Actor', secondary = 'actor_roles')
+
+class ActorRole(Base):
+    __tablename__ = 'actor_roles'
+
+    actor_id = Column(Integer, ForeignKey('actors.id'), primary_key = True)
+    role_id = Column(Integer, ForeignKey('roles.id'), primary_key = True)
+
+engine = create_engine('sqlite:///actor_roles.db')
 Base.metadata.create_all(engine)
--- queries.py
+++ queries.py
@@ -1,13 +1,23 @@
 from models import *
 
+session = sessionmaker()
+session.configure(bind=engine)
+Base.metadata.bind = engine
+
+session = session()
+
 def return_christian_bales_roles(session):
-    pass
+    return session.query(Actor).filter(Actor.name == 'Christian Bale').first().roles
     # Return a list of Christian Bale role instances
 
+    # role_objects = session.query(Actor).filter(Actor.name == 'Christian Bale').first().roles
+    # return list(map(lambda x: x.character, role_objects))
+    # Return a list of Christian Bale role names
+
 def return_catwoman_actors(session):
-    pass
+    return session.query(Role).filter(Role.character == 'Catwoman').first().actors
     #  Return a list of actor instances that have played Catwoman
 
 def return_number_of_batman_actors(session):
-    pass
+    return len(session.query(Role).filter(Role.character == 'Batman').first().actors)
     # Return the number of actors that have played Batman
